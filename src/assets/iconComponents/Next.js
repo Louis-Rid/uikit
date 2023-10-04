5201a9e210373574825e703802e2715bbce025eb
Louis-Rid <131224467+Louis-Rid@users.noreply.github.com>
2023-10-04 12:19:15 -0600

    added svgr to convert icons to react components

---
+++ src/assets/iconComponents/Next.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+const SvgNext = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={49}
+    height={50}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#000"
+      fillRule="evenodd"
+      d="M37.855 21.159V2.597c0-.918.714-1.665 1.696-1.665h6.964c.893 0 1.607.747 1.607 1.665v44.809c0 .918-.714 1.664-1.607 1.664h-6.964c-.982 0-1.696-.746-1.696-1.664V28.844L3.928 48.44a2.594 2.594 0 0 1-2.589 0C.536 47.98 0 47.126 0 46.203V3.799c0-.922.536-1.775 1.34-2.236a2.596 2.596 0 0 1 2.588-.001l33.927 19.597Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgNext;
