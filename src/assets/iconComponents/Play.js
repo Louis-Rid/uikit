4fa6c1ce97906048686788862fb4bc213b327e71
Louis-Rid <131224467+Louis-Rid@users.noreply.github.com>
2023-10-09 13:58:22 -0600

    Simplified implementation of icons on buttons

---
+++ src/assets/iconComponents/Play.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+
+const SvgPlay = ({ iconHeight, iconWidth, ...props }) => (
+    <svg role="img" height={iconHeight} width={iconHeight} aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>
+);
+export default SvgPlay;
