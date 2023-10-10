61bd2aff576e73892c050118061b1a104e7701b2
Louis-Rid <131224467+Louis-Rid@users.noreply.github.com>
2023-10-10 07:31:54 -0600

    Added icons for NavTabs

--- src/assets/iconComponents/index.js
+++ src/assets/iconComponents/index.js
@@ -1,2 +1,6 @@
 export { default as Download } from "./Download";
+export { default as Home } from "./Home";
+export { default as HomeFilled } from "./HomeFilled";
+export { default as Search } from "./Search";
+export { default as SearchFilled } from "./SearchFilled";
 export { default as Play } from "./Play";
