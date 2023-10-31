0bca04efb179c19e5a744cb0a7616904bfa2c6e5
Louis-Rid <131224467+Louis-Rid@users.noreply.github.com>
2023-10-31 08:40:00 -0600

    Added Svgs for top elements on sidebar

---
+++ src/assets/iconComponents/Arrow.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+
+const SvgArrow = ({ dimensions, ...props }) => (
+    <svg role="img" height={dimensions} width={dimensions} aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon"><path d="M7.19 1A.749.749 0 0 1 8.47.47L16 7.99l-7.53 7.521a.75.75 0 0 1-1.234-.815.75.75 0 0 1 .174-.243l5.72-5.714H.75a.75.75 0 1 1 0-1.498h12.38L7.41 1.529a.749.749 0 0 1-.22-.53z"></path></svg>
+);
+export default SvgArrow;
--- src/assets/iconComponents/index.js
+++ src/assets/iconComponents/index.js
@@ -2,3 +2,6 @@ export { default as Download } from "./Download";
 export { default as Play } from "./Play";
 export { default as Home } from "./Home";
 export { default as Search } from "./Search";
+export { default as Library } from "./Library";
+export { default as Plus } from "./Plus";
+export { default as Arrow } from "./Arrow";
